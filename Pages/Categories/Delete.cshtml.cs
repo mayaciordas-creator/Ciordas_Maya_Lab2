a4bc81865d199ad0f2b80be02f1a26bbcf4f37e6
MAYA-NATALIA CIORDAȘ <maya.ciordas@icloud.com>
2025-11-02 13:42:58 +0200

    svvf

---
+++ Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Ciordas_Maya_Lab2.Data;
+using Ciordas_Maya_Lab2.Models;
+
+namespace Ciordas_Maya_Lab2.Pages.Categories
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly Ciordas_Maya_Lab2.Data.Ciordas_Maya_Lab2Context _context;
+
+        public DeleteModel(Ciordas_Maya_Lab2.Data.Ciordas_Maya_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Category Category { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Category.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Category = category;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Category.FindAsync(id);
+            if (category != null)
+            {
+                Category = category;
+                _context.Category.Remove(Category);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
